03c8fc3dff5a84ab75fdcd8fcc4be674ab452d20
Rajukumar <rajuk631149@gmail.com>
2026-08-03 15:49:34 +0530

    Day -04

---
+++ Lecture-04/Incrementoperator.js
@@ -0,0 +1,51 @@
+// // Assignment operator 
+// let score = 100 ; 
+
+// let b = 150;
+// score-=b
+// console.log(score);
+
+
+// Post and prefix 
+// Increment ++ 
+// Decrement --
+
+// let score = 5;
+// console.log(++score); //value phale increment kar dha //Prefix increment 
+// console.log(score++); // value 5 uska increment hoga // post incrment 
+
+let postfix = 5; // phale tu 5 show kar gha print kar watha phir increment hoga => i = i+1; 
+let prefix = 5;
+
+// console.log(postfix++); // Prints 5.=> 5+1=> 6 `postfix` is now 6.
+// console.log(postfix);   // Prints 6.
+
+
+// // phale yi increment kar 5+1 = > 6 
+// console.log(++prefix); // Prints 6. `prefix` is now 6.
+// console.log(prefix);   // Prints 6.
+
+
+// console.log(postfix--); // Prints 5.=> 5-1=> 4 `postfix` is now 6.
+// console.log(postfix);   // Prints 4.
+
+
+// // phale yi increment kar 5-1 = > 4 
+// console.log(--prefix); // Prints 4. `prefix` is now 6.
+// console.log(prefix);   // Prints 4.
+
+
+
+
+// loops 
+// for(let i = 0 ; i <= 5; ++i){
+//     console.log(i);
+// }
+
+
+
+// 0 1  2  3 4 5 
+// 5 4 3 2 1 ;
+// for(let i=5; i>=0; i--){
+//     console.log(i)
+// }
